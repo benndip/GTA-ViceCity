5e1e95264fb613001a1a4b107186c8a7202c2729
benndip <obentabiayuk1@gmail.com>
2025-03-12 09:22:36 +0100

    Test spawning of AiCharacters and also implement animations for shotting rifles and pistols

---
+++ Assets/Scripts/AiCharacters/AiSpawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class AiSpawner : MonoBehaviour
+{
+    public GameObject[] AiPrefabs;
+    public int AiToSpawn;
+    public int Timer;
+
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        int count = 0;
+
+        while(count < AiToSpawn)
+        {
+            int randomIndex = Random.Range(0, AiPrefabs.Length);
+
+            GameObject obj = Instantiate(AiPrefabs[randomIndex]);
+
+            Transform child = transform.GetChild(Random.Range(0, transform.childCount - 1));
+
+            obj.GetComponent<WaypointNavigator>().currentWaypoint = child.GetComponent<Waypoint>();
+            obj.transform.position = child.position;
+
+            yield return new WaitForSeconds(Timer);
+
+            count++;
+        }
+    }
+
+}
